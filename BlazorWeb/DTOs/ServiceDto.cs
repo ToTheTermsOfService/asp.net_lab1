9fe6e8e4206529611767a593de7ee94d90533ec2
ZhalobaPav <zhalobapavlo@gmail.com>
2025-11-20 00:18:43 +0200

    blazor

---
+++ BlazorWeb/DTOs/ServiceDto.cs
@@ -0,0 +1,11 @@
+﻿namespace WEB.DTOs
+{
+    public class ServiceDto
+    {
+        public int Id { get; set; }
+        public string ServiceName { get; set; }
+        public string BillingType { get; set; }
+        public decimal Tariff { get; set; }
+        public decimal CalculatedAmount { get; set; } = 0;
+    }
+}
